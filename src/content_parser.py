f04fa074cab0ee11ca1aefe983024d0652c72fc0
IvanPragma <ip20060226@gmail.com>
2022-06-07 21:34:24 +0500

    Parser of useful content
    - add parser
    - add support for image url
    - add clear text wrap

--- src/content_parser.py
+++ src/content_parser.py
@@ -1,7 +1,30 @@
 from __future__ import annotations
 
+import requests
+from bs4 import BeautifulSoup
+
+from tools import http_in_url_check
+
 
 def parse_useful_content(url: str, parse_images: bool) -> str:
     """Parse useful site content by url."""
 
-    return f'This like useful content for url "{url}"'
+    url = http_in_url_check(url)
+
+    try:
+        page = requests.get(url)
+    except requests.exceptions.MissingSchema:
+        return 'INVALID URL'
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    if parse_images:
+        for img in soup.find_all('img'):
+            img_url = img.attrs.get('src')
+            if img_url.startswith('/'):
+                # For url without 'http://'
+                img_url = img_url[1:]
+                img_url = url + img_url
+            img.string = img_url
+
+    useful_content = soup.get_text(' ', strip=True)
+    return useful_content
--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import textwrap
 from typing import Union
 
 yes_particles = 'yes yeah ye y да д true 1'.split()
@@ -18,4 +19,23 @@ def user_friendly_bool_arg_parse(value: Union[str, bool]) -> bool:
 def break_line(text: str, line_width: int = 80) -> str:
     """Break line by max line width."""
 
-    return text
+    # Remove double whitespaces
+    while '  ' in text:
+        text = text.replace('  ', ' ')
+
+    return textwrap.fill(text, width=line_width)
+
+
+def http_in_url_check(url: str) -> str:
+    """Check 'http://' in url and add it if does not exists."""
+
+    if url.startswith('/'):
+        url = url[1:]
+
+    if not (url.startswith('http://') or url.startswith('https://')):
+        url = 'http://' + url
+
+    if not url.endswith('/'):
+        url += '/'
+
+    return url
