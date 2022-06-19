3e0140d5f683ad83b22fcff1703d4da232de69ec
IvanPragma <ip20060226@gmail.com>
2022-06-19 13:12:53 +0500

    Cleaning the page from garbage
    - using readability (arc90's) to clean html page
    - add headers to request
    - br tags are now treated as "\n"

--- src/content_parser.py
+++ src/content_parser.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import requests
 from bs4 import BeautifulSoup
+from readability import Document
 
 from tools import http_in_url_check
 
@@ -12,10 +13,17 @@ def parse_useful_content(url: str, parse_images: bool) -> str:
     url = http_in_url_check(url)
 
     try:
-        page = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                          'AppleWebKit/537.36 (KHTML, like Gecko)'
+                          'Chrome/67.0.3396.62 Safari/537.36 OPR/54.0.2952.64'
+        }
+        page = requests.get(url, headers=headers)
     except requests.exceptions.MissingSchema:
         return 'INVALID URL'
-    soup = BeautifulSoup(page.text, "html.parser")
+    doc = Document(page.text,
+                   min_text_length=20)
+    soup = BeautifulSoup(doc.summary(), "html.parser")
 
     if parse_images:
         for img in soup.find_all('img'):
@@ -26,5 +34,8 @@ def parse_useful_content(url: str, parse_images: bool) -> str:
                 img_url = url + img_url
             img.string = img_url
 
-    useful_content = soup.get_text(' ', strip=True)
+    for br in soup.find_all('br'):
+        br.string = '\n'
+
+    useful_content = soup.get_text(' ')
     return useful_content
